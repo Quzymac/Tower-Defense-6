a6fac520cf45cc70e0b8c6ad54de636c74f50606
Flippeen <filip.k.lindgren@gmail.com>
2017-11-13 16:10:52 +0100

    Working

---
+++ Tower Defence 2.0/Assets/Scripts/Enemy.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+
+    public Transform targetPoint;
+
+    public int health = 5;
+    [SerializeField] int damageTakenFromCanon = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        transform.GetComponent<NavMeshAgent>().destination = targetPoint.position; //stops on arrival
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (health == 3)
+        {
+            gameObject.GetComponent<Renderer>().material.color = new Color32(255, 255, 0, 0);
+        }
+        if (health == 4)
+        {
+            gameObject.GetComponent<Renderer>().material.color = new Color32(255, 0, 255, 0);
+        }
+        if (health == 2)
+        {
+            gameObject.GetComponent<Renderer>().material.color = new Color32(255, 0, 0, 255);
+        }
+        if (health == 1)
+        {
+            gameObject.GetComponent<Renderer>().material.color = new Color32(255, 255, 255, 0);
+        }
+
+
+        if (health < 1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+
+        if (collider.CompareTag("Bullet"))
+        {
+            TakeDamage(damageTakenFromCanon);
+        }
+    }
+
+    void TakeDamage(int Damage)
+    {
+        health -= Damage;
+    }
+}
+
+
---
+++ Tower Defence 2.0/Assets/Scripts/IfCanWalk.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfCanWalk : MonoBehaviour {
+
+    private Transform _myTransform = null;
+    private Vector3 _lastPosition;
+
+    private void Awake()
+    {
+        _myTransform = transform;
+        _lastPosition = _myTransform.position;
+    }
+
+    private void Update()
+    {
+        if (_lastPosition == _myTransform.position)
+        {
+            Debug.Log("Did not move");
+        }
+        else
+        {
+            Debug.Log("Moved");
+        }
+        _lastPosition = _myTransform.position;
+    }
+}
