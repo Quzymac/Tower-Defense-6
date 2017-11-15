57cdeedc3fdd84b122f9bff51727bdb91fcd9b9f
Quzymac <33623813+Quzymac@users.noreply.github.com>
2017-11-15 13:22:30 +0100

    healthbar rotation fix

---
+++ Tower Defence 2.0/Assets/Scripts/HealthBarRotationFix.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarRotationFix : MonoBehaviour {
+
+    int XRotation = 90; //locking the rotation at a certain angle for X
+    int YRotation; //locking the rotation at a certain angle for Y
+    int ZRotation; //locking the rotation at a certain angle for Z
+ 
+ 
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(XRotation, YRotation, ZRotation);
+    }
+}
